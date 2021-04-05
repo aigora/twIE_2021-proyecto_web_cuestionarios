8e01d25896908f9669781c6f751dc0e251dc587d
Javierdelahoz4 <79969759+Javierdelahoz4@users.noreply.github.com>
2021-04-05 16:57:33 +0200

    Update Alfa.c

--- Alfa.c
+++ Alfa.c
@@ -1,10 +1,55 @@
 #include <stdio.h>
 
-int main()
-
+int main ()
 {
-  
-  printf("¡BIENVENIDO A LA WEB DE CUESTIONARIOS!\n");
 
- return 0;
+	char cuestionario,F,f, opcion;
+	printf("       ¡¡BIENVENIDO A LA MEJOR PAGINA PARA HACER CUESTIONARIOS!!     \n\n\n");
+    do
+    {
+
+    printf("A continuacion seleccione la opcion deseada pulsando la tecla correspondiente\n\n");
+    printf(" 0-Menu general de test e intrucciones\n\n 1-Cuestionario 1\n 2-Cuestionario 1\n 3-Cuestionario 1\n 4-Cuestionario 1\n\n 5-Salir de la pagina\n");
+	scanf("%c",&opcion);
+
+	switch(opcion) {
+		case '0' :
+			printf("Menu general de test e intrucciones\n");
+			printf("En esta página de cuestionarios tiene la posibilidad de realizar distintos cuestionarios que en funcion de las respuestas te dara unos reulstados. Puede elegir entre:\n");
+            printf("Cuestionario de personalidad.\n Cuestionario de inteligencia.\n Cuestionario de moda.\n Cuestionario de cultura general.\n Cuestionario de politica.\n\n\n");
+            printf("Pulse 'f' para volver al menu principal");
+            scanf("&c", f);
+
+			break;
+		case '1' :
+			printf("Cuestionario 1\n\n\n");
+			printf("Pulse 'f' para volver al menu principal\n");
+			scanf("&c", f);
+			break;
+		case '2' :
+			printf("Cuestionario 2\n\n\n");
+			printf("Pulse 'f' para volver al menu principal\n");
+			scanf("&c", f);
+			break;
+		case '3' :
+			printf("Cuestionario 3\n\n\n");
+			printf("Pulse 'f' para volver al menu principal\n");
+			scanf("&c", f);
+			break;
+		case '4' :
+			printf("Cuestionario 4\n\n\n");
+			printf("Pulse 'f' para volver al menu principal\n");
+			scanf("&c", f);
+			break;
+        case '5' :
+            printf("Ha pulsado 'salir de la pagina'.");
+            printf(" ¡¡Esperamos que le haya gustado!!");
+            break;
+        default :
+			printf("El numero introducido no corresponde con ninguna opcion, por favor seleccione otro numero");
+			printf("Pulse 'f' para volver al menu principal\n");
+			scanf("&c", f);
 }
+    }
+    while(opcion!='5');
+return 0;}
