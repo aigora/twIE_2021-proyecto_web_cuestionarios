fde4caf72617a2b6ae02aa75ee790e9c148b3519
inigoazqueta <inigo.azqueta@alumnos.upm.es>
2021-05-10 14:10:07 +0200

    Estructuras soluciones
    Estructuras generales para leer las respuestas y determinar si son correctas

---
+++ Paises2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+   int x;
+   int res[5];
+   printf("Elige la respuesta\n");
+   scanf("%d", res);
+  if (res[0]==55)
+        x=+1;
+    
+ printf("%d\n",x);
+
+    
+    return 0;
+}
---
+++ cult1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  int x=0;
+  char c;
+  scanf("%c",&c);
+  if (c==99)
+   x=+1;
+  printf("%d\n",x);
+    return 0;
+}
