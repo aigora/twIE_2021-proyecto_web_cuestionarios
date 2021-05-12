bd76211c3a0c58442b7048e00c4b9a23deb19f1f
Nachofdez2708 <79969508+Nachofdez2708@users.noreply.github.com>
2021-05-12 11:35:02 +0200

    Update CodSoluciones.c

--- CodSoluciones.c
+++ CodSoluciones.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-int SolConducir (int x);
-int SolDeporte  (int x);
-int SolCultura  (int x);
-int SolPaises  (int x);
-int SolPersonalidades (int a,int b, int c,int d);
+#include"CodSoluciones.h"
 
 
 int SolConducir (int x)
