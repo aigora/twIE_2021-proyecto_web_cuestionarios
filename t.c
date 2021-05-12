cdef4ed36e4685ccbe49648e2af3f762a99e2761
inigoazqueta <inigo.azqueta@alumnos.upm.es>
2021-05-12 11:47:28 +0200

    Mejora codigo

--- t.c
+++ t.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define N 30
 
 typedef struct
 {
-  char texto[150];
-} pregunta;
+  char pregunta[100];
+  char opciones[50];
+  char soluciones[30];
+  char respuestas[30];
+} pregunta,opciones,soluciones,respuestas;
 
 int main()
 {
   pregunta preguntas[N];
-  FILE *fpreguntas;
-  int i = 0;
+  opciones opciones[N];
+  soluciones soluciones[N];
+  respuestas respuestas[N];
+  FILE *fpreguntas,*frespuestas,*fsoluciones;
+  int i = 0,x=0;
 
   fpreguntas = fopen("Cultura_Preguntas.txt", "r");
-    
-  while(fscanf(fpreguntas, "%[^\n]\n", preguntas[i].texto) != EOF)
+  fopciones = fopen("Cultura_Opciones.txt", "r");
+  fsoluciones = fopen("Cultura_Soluciones.txt", "r");
+  while(fscanf(fpreguntas, "%[^\n]\n %[^;]\n %[^;]\n %[^;]\n", preguntas[i].pregunta,opciones[i].opciones,opciones[i].opciones,) != EOF)
     {
-      printf("%s\n", preguntas[i].texto);
+      printf("%s\n%s\n%s\n%s\n", preguntas[i].pregunta,opciones[i].opciones,opciones[i].opciones,opciones[i].opciones);
+      
+      
+      
       i++;
     }
   
