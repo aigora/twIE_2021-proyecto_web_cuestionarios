85aca0880f205977723010b01f7797e5b70e42f4
inigoazqueta <inigo.azqueta@alumnos.upm.es>
2021-05-12 12:40:43 +0200

    probando

--- t.c
+++ t.c
@@ -18,22 +18,24 @@ int main()
   int i = 0,x=0;
   char n,s,soluciones[30];
   fsoluciones = fopen("Cultura_Soluciones.txt", "r");
-  
+   
   
   fpreguntas = fopen("Cultura_Preguntas.txt", "r");
   fopciones = fopen("Cultura_Opciones.txt", "r");
-  while(fscanf(fpreguntas, "%[^\n]\n %[^;]\n %[^;]\n %[^;]\n", cuestionario[i].preguntas,cuestionario[i].opciones,cuestionario[i].opciones, cuestionario[i].opciones) != EOF)
+  while(fscanf(fpreguntas, "%[^\n]\n", vector[i].preguntas) && fscanf(fopciones,"%[^;]\n %[^;]\n %[^;]\n",vector[i].opciones,vector[i].opciones, vector[i].opciones) != EOF)
     {
-      printf("%s\n%s\n%s\n%s\n",cuestionario[i].preguntas,cuestionario[i].opciones,cuestionario[i].opciones,cuestionario[i].opciones);
-      s=soluciones[i];
+      printf("%s\n%s\n%s\n%s\n",vector[i].preguntas,vector[i].opciones,vector[i].opciones,vector[i].opciones);
+      /*s=soluciones[i];
       scanf("%c",&n);
       if (n==s)
-        x=+1;
+        x=+1;*/
       
       
       i++;
     }
   
   fclose(fpreguntas);
+  fclose(fopciones);
+  fclose(fsoluciones);
   return 0;  
 }
