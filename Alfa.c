0e9bcef461bb1b21f51bdfe5378e9b48bc2c9603
inigoazqueta <inigo.azqueta@alumnos.upm.es>
2021-05-21 12:53:34 +0200

    Update Alfa.c

--- Alfa.c
+++ Alfa.c
@@ -8,37 +8,37 @@ int main()
     do
     {
         printf("\n\n  En esta página tiene la posibilidad de realizar cuestionarios/tests de distintos temas. Puede elegir entre:\n\n\n\n");
-        printf("     1-Cuestionario de personalidad.\n     2-Test de cultura general.\n     3-Test de Conducir.\n     4-Cuestionario de Capitales intermacionales.\n     5-Cuestionario de Deportes.\n     6-Salir\n\n");
+        printf("     1-Cuestionario de personalidad.\n     2-Test de cultura general.\n     3-Test de Conducir.\n     4-Cuestionario de Geografía.\n     5-Cuestionario de Deportes.\n     6-Salir\n\n");
         printf("\n\n\n  A continuacion seleccione la opcion deseada pulsando la tecla correspondiente:\n\n");
         printf("   Introduzca el numero:");scanf(" %c",&opcion);
     switch (opcion)
     {
           case '1':
-            printf("\n   Cuestionario 1\n\n\n");
+            printf("\n  Cuestionario de personalidad\n\n\n");
             printf("   Pulse 'f' para volver al menu principal\n");
             scanf(" %c", &f);
             break;
 
           case '2':
-            printf("\n   Cuestionario 2\n\n\n");
+            printf("\n   Test de cultura general\n\n\n");
             printf("   Pulse 'f' para volver al menu principal\n");
             scanf(" %c", &f);
             break;
 
           case '3':
-            printf("\n   Cuestionario 3\n\n\n");
+            printf("\n   Test de Conducir\n\n\n");
             printf("   Pulse 'f' para volver al menu principal\n");
             scanf(" %c", &f);
             break;
 
           case '4':
-            printf("\n   Cuestionario 4\n\n\n");
+            printf("\n   Cuestionario de Geografía\n\n\n");
             printf("   Pulse 'f' para volver al menu principal\n");
             scanf(" %c", &f);
             break;
 
           case '5':
-            printf("\n   Cuestionario 5\n\n\n");
+            printf("\n   Cuestionario de Deportes\n\n\n");
             printf("   Pulse 'f' para volver al menu principal\n");
             scanf(" %c", &f);
             break;
