947d9e85ce4b1737400eae8fb2778aaba8b96b2d
Javierdelahoz4 <79969759+Javierdelahoz4@users.noreply.github.com>
2021-05-23 18:43:58 +0200

    Conducir

--- Preguntas.h
+++ Preguntas.h
@@ -7,6 +7,7 @@
 
 int Personalidad_definitivo (int a,int b,int c,int d);
 int Cultura_definitivo (int x);
+int Conducir_definitivo (int x);
 
 
 
@@ -98,4 +99,39 @@ typedef struct
   fclose(fsoluciones);
   SolCultura (x);
   return x;  
+}
+
+int Conducir_definitivo (int x){
+typedef struct
+{
+  char preguntas[200];
+  char opciones[150];
+  char soluciones;
+} cuestionario;
+
+
+  cuestionario vector[N];
+  FILE *fpreguntas,*fopciones,*fsoluciones;
+  int i = 0;
+  char n;
+  
+  x=0;
+  fsoluciones = fopen("Conducir_Soluciones.txt","r");
+  fpreguntas = fopen("Conducir_Preguntas.txt", "r");
+  fopciones = fopen("Conducir_Opciones.txt", "r");
+  while(fscanf(fpreguntas, "%[^\n]\n", vector[i].preguntas) !=EOF && fscanf(fopciones,"%[^\n]\n",vector[i].opciones) != EOF && fscanf(fsoluciones,"%[^\n]\n",&vector[i].soluciones) !=EOF)
+    {
+      printf("%s\n\n%s\n\n",vector[i].preguntas,vector[i].opciones);
+
+      scanf(" %c",&n);
+      if (n==vector[i].soluciones)
+        x++;
+
+       i++;
+    }
+
+  fclose(fpreguntas);
+  fclose(fopciones);
+  fclose(fsoluciones);
+  return x;
 }
