b517a8a276f4e062fe46f7e5aa1d0b9ba649d17d
inigoazqueta <inigo.azqueta@alumnos.upm.es>
2021-05-23 18:32:20 +0200

    Update CodSoluciones.h

--- CodSoluciones.h
+++ CodSoluciones.h
@@ -9,6 +9,7 @@ int SolDeporte  (int x);
 int SolCultura  (int x);
 int SolPaises  (int x);
 int SolPersonalidades (int a,int b, int c,int d);
+int Personalidad_definitivo (int a,int b,int c,int d);
 
 int SolConducir (int x)
 {
@@ -75,3 +76,55 @@ int SolPersonalidades (int a,int b, int c,int d)
   return 0;
 
 }
+
+int Personalidad_definitivo (int a,int b,int c,int d){
+typedef struct
+{
+  char preguntas[200];
+  char opciones[300];
+  char soluciones;
+} cuestionario;
+
+
+  cuestionario vector[U];
+  FILE *fpreguntas,*fopciones,*fsoluciones;
+  int i = 0;
+  char letra;
+   
+   printf("\t\t\tTEST DE PERSONALIDAD\nDEPENDIENDO DE SUS RESPUESTAS, SABRA QUE TIPO DE PERSONALIDAD TIENE\n\n\tESTE TEST ESTA DIVIDO EN DOS PARTES:\n\t SOCIAL/TIMIDO Y EGOCENTRICO/ALTRUISTA\n\n");
+   printf("PORFAVOR RESPONDA CON SINCERIDAD\n\tTODAS LAS RESPUESTAS DEBERAN ESTAR EN MINUSCULA PARA SER VALIDADAS\n\n");
+   a=0; 
+   b=0;
+   c=0;
+   d=0; 
+
+
+  fsoluciones = fopen("Personalidad_Soluciones.txt","r");
+  fpreguntas = fopen("Personalidad_Preguntas.txt", "r");
+  fopciones = fopen("Personalidad_Opciones.txt", "r");
+  while(fscanf(fpreguntas, "%[^\n]\n", vector[i].preguntas) !=EOF && fscanf(fopciones,"%[^\n]\n",vector[i].opciones) != EOF && fscanf(fsoluciones,"%[^\n]\n",&vector[i].soluciones) !=EOF)
+    {
+      printf("%s\n\n%s\n\n",vector[i].preguntas,vector[i].opciones);
+      
+      scanf(" %c", &letra);
+          if(letra==97)
+             a++;
+
+        else if(letra==98)
+            b++;
+
+        else if(letra==99)
+            c++;
+
+        else if(letra==100)
+            d++;
+        else
+            printf("Lo sentimos, esa respuesta no es valida");
+    }
+  
+  fclose(fpreguntas);
+  fclose(fopciones);
+  fclose(fsoluciones);
+        return a,b,c,d;
+ }
+
